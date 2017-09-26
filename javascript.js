b41556288208508f12cc2eb82334ed9f72210d93
Sara Javed <sarajaved@Saras-Air.fios-router.home>
2017-09-25 21:35:33 -0400

    cleaned up code and pseudocode

--- javascript.js
+++ javascript.js
@@ -1,99 +1,119 @@
-//Animal array
+
+//---------- GLOBAL VARIABLES ----------//
+
+
+//animalArray holds original array of animal choices
 var animalArray = ["Sloth", "Hamster", "Kitty"];
 console.log(animalArray);
 
-//Function for displaying animal buttons
+
+//---------- FUNCTIONS ----------//
+
+
+//renderButtons
+//function for displaying animal buttons
 function renderButtons() {
 
-  // Deleting the buttons prior to adding new movies
-  // (this is necessary otherwise you will have repeat buttons)
+  //div empties out and removes all buttons
   $("#animalButtons").empty();
 
-  // Looping through the array of movies
+  //loop through array of animals to account for new additions
+  //loop through array of animals to add buttons to div
   for (var i = 0; i < animalArray.length; i++){
-    // Then dynamicaly generating buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    //create button tag for each button
     var button = $("<button>");
-    // Adding a class of movie to our button
+    //animal class added to each button
     button.addClass("animal");
-    // Adding a data-attribute
+    //name of the animal is added to each button as data-name
     button.attr("data-name", animalArray[i]);
-    // Providing the initial button text
+    //name of animal is displayed as the text of each button
     button.text(animalArray[i]);
-    // Adding the button to the buttons-view div
+    //each new button is appended to the div
     $("#animalButtons").append(button);
   }
 
 };
 
-renderButtons();
 
-// Function for dumping the JSON content for each button into the div
+//displayImages
+//function to display images associated with the data-name of button selected
 function displayImages() {
 
+  //div empties out and removes all images 
   $("#animals").empty();
-
+  //animal variable stores data-name of the button selected
 	var animal = $(this).attr("data-name");
   console.log(animal);
+
+  //queryURL
+  //search changes based on animal variable (data-name)
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+animal+"&api_key=dc6zaTOxFJmzC&rating=pg";
-  
+        //ajax call
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
 
           console.log(response);
-          
+          //loop through 10 times to generate 10 images
           for (var i = 0; i < 10; i++){
-            
-            // Saving the image_original_url property
-            var imageUrl = response.data[i].images.original_still.url;
-
-            // Creating and storing an image tag
+            //imageURL stores the url of the still image of gif
+            var imageURL = response.data[i].images.original_still.url;
+            //image tag is created to hold image
             var animalImage = $("<img>");
-
-            // Setting the catImage src attribute to imageUrl
-            animalImage.attr("src", imageUrl);
-            //animalImage.attr("alt", animal);
+            //image tag receives src attribute
+            //src = imageURL
+            animalImage.attr("src", imageURL);
+            //image tag receives alt attribute
+            //alt = animal
             animalImage.attr("alt", animal);
-
+            //image tag receives size attribute
+            //width = 30% of container
             animalImage.attr("width", "30%");
-
-            // Prepending the catImage to the images div
+            //images are prepened to the animals div
             $("#animals").prepend(animalImage);
-
           }
-
+          //populate buttons
+          //call renderButtons function
           renderButtons();
           
-
         });
 
 };
 
-// This function handles events where one button is clicked
+
+//---------- CLICK EVENTS ----------//
+
+
+//when user inputs an animal, it is added as a button
+//when addAnimal submit button is clicked:
 $("#addAnimal").on("click", function(event) {
+
     event.preventDefault();
 
-    // This line grabs the input from the textbox
+    //animal variable stores input value from user
     var animal = $("#animal-input").val().trim();
 
-    // Adding the movie from the textbox to our array
-    animalArray.push(animal);
     console.log(animal)
 
-    // Calling renderButtons which handles the processing of our movie array
+    //animal (input value) is pushed into the animalArray
+    animalArray.push(animal);
+    
+    //populate buttons
+    //call renderButtons function
     renderButtons();
+
 });
 
 
-// Function for displaying the movie info
-//Using $(document).on instead of $(".movie").on to add event listenersto dynamically generated elements
+//on the window:
+//when any button with animal class is clicked, displayImages function is called
 $(document).on("click", ".animal", displayImages);
 
-// Calling the renderButtons function to display the intial buttons
+
+//---------- ON PAGE LOAD ----------//
+
+
+//populate buttons
+//call renderButtons function
 renderButtons();
-
-
-
-
